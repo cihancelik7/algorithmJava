629f1d09f13be81fae5a77d9516da90baa889ab4
cihancelik7 <cihan.celik7@gmail.com>
2024-04-27 20:55:17 +0300

    Donguler yedinci soru, girilen5NotlarinKareVeKokleri

---
+++ src/AlgoritmaTasarimiVeGelistirme/Donguler/Soru_7.java
@@ -0,0 +1,25 @@
+package AlgoritmaTasarimiVeGelistirme.Donguler;
+
+import java.util.Scanner;
+
+/*
+10 kisiye ait notlarin karelerini ve kuplerini yazdiran bir program.
+ */
+public class Soru_7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int not,kisi;
+        float kare,kup;
+
+
+        System.out.print("Kisi sayisini giriniz:");
+        kisi = scan.nextInt();
+        for (int i =1; i<kisi; i++){
+            System.out.print("Notu giriniz:");
+            not = scan.nextInt();
+            kare = not*not;
+            kup = kare * not;
+            System.out.println(i+". kisinin notu: "+not+" Karesi: "+kare+" Kupu: "+kup);
+        }
+    }
+}
